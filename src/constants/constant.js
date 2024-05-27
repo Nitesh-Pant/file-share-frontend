956c879fc71f741318c5bc11703678b3078fe571
NiteshPant <nitesh.pant@xlscout.com>
2024-05-27 23:51:21 +0530

    url updated
    updated url port

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,4 +1,4 @@
-const UPLOAD_URL = process.env.UPLOAD_URL || 'http://43.205.62.246:3000/api/upload'
+const UPLOAD_URL = process.env.UPLOAD_URL || 'http://43.205.62.246:8000/api/upload'
 export default {
     UPLOAD_URL
 };
